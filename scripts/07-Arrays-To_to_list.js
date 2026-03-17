c63b0e6e7e0372d409cee93c98be51a38f19448a
awaismalik8107 <awaismalik8107@gmail.com>
2026-03-17 23:55:36 +0500

    07-Arrays

--- scripts/07-Arrays-To_to_list.js
+++ scripts/07-Arrays-To_to_list.js
@@ -10,7 +10,21 @@ let todolistHTML='';
 for(let i=0;i<to_do_list.length;i++)
 {
   const todo=to_do_list[i];
-  const html=`<p>${todo}</p>`;//generating html
+  // const name=todo.name;
+  // // const date=todo.date;
+
+  const { name,date }=todo;//Destructring;
+
+  const html=`
+  <p>
+    ${name} ${date}<button
+    onclick=
+    "
+    to_do_list.splice(${i},1);
+    render();
+    "
+    >Delete</button>
+  </p>`;//generating html
   todolistHTML += html;
 }
 
@@ -19,6 +33,10 @@ document.querySelector('.printer').innerHTML=todolistHTML;//we did this because 
 function addTodo()
 {
   const input_element=document.querySelector('.js_input');
+  const date_input=document.querySelector('.Date');
+
+
+
   const name =input_element.value;
    console.log(input_element);
   console.log(name);
