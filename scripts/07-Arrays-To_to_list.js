4e7b80f4838bd35310d6c6c1ca772b3e1fb2da77
awaismalik8107 <awaismalik8107@gmail.com>
2026-03-17 22:59:33 +0500

    07-Arrays

--- scripts/07-Arrays-To_to_list.js
+++ scripts/07-Arrays-To_to_list.js
@@ -1,4 +1,21 @@
 const to_do_list=[];
+
+render();
+
+
+function render()
+{
+let todolistHTML='';
+
+for(let i=0;i<to_do_list.length;i++)
+{
+  const todo=to_do_list[i];
+  const html=`<p>${todo}</p>`;//generating html
+  todolistHTML += html;
+}
+
+document.querySelector('.printer').innerHTML=todolistHTML;//we did this because html does support <p> nesting
+}
 function addTodo()
 {
   const input_element=document.querySelector('.js_input');
@@ -10,4 +27,6 @@ function addTodo()
   console.log(to_do_list);
 
   input_element.value='';
+  render();
 }
+
